82245b05951608463fe6e40bb95c9684100e9390
NikolaDziwusz <nikoladziwusz@gmail.com>
2025-07-01 03:21:40 +0200

    chore: add pnpm workspace configuration and update build options in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/memo-game/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   plugins: [react()],
   css: {
     preprocessorOptions: {
